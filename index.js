017874f902b9080c682ace95f2996555288d57bd
Ryan Best <ryan@Ryans-MacBook-Pro.local>
2017-10-13 14:36:54 -0500

    Added bootstrap.js and updated JavaScript

--- index.js
+++ index.js
@@ -10,14 +10,8 @@ var SubButtons = function() {
   document.getElementById("sumNumbers").innerHTML = a-b;
 }
 
-var MulButtons = function() {
-  var a = parseInt(document.getElementById("InputNumber1").value);
-  var b = parseInt(document.getElementById("InputNumber2").value);
-  document.getElementById("sumNumbers").innerHTML = a*b;
-}
-
-var DivButtons = function() {
-  var a = parseInt(document.getElementById("InputNumber1").value);
-  var b = parseInt(document.getElementById("InputNumber2").value);
-  document.getElementById("sumNumbers").innerHTML = a/b;
+var RollDice = function myFunction() {
+  var NumberOfSides;
+  var x = Math.floor((Math.random() * 10) + 1);
+  document.getElementById("demo").innerHTML = x;
 }
