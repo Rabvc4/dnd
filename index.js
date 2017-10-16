1528c2caf68a1cf284669b8f9f1b1f12946c0487
Rabvc4 <chill_zero@hotmail.com>
2017-10-16 12:45:29 -0500

    Added InputText function for parsing text

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ var SumButtons = function() {
   document.getElementById("sumNumbers").innerHTML = a+b;
 }
 
-var SubButtons = function() {
-  var a = parseInt(document.getElementById("InputNumber1").value);
-  var b = parseInt(document.getElementById("InputNumber2").value);
-  document.getElementById("sumNumbers").innerHTML = a-b;
+var RollDice = function() {
+  var NumberOfSides = parseInt(document.getElementById("InputNumber1").value);
+  var RollResult = Math.floor((Math.random() * NumberOfSides) + 1);
+  document.getElementById("rollResult").innerHTML = RollResult;
 }
 
-var RollDice = function myFunction() {
-  var NumberOfSides;
-  var x = Math.floor((Math.random() * 10) + 1);
-  document.getElementById("demo").innerHTML = x;
+var InputText = function() {
+  var UserInput = document.getElementById("InputNumber1").value;
+  var RollArray = UserInput.split("d");
+  var NumberOfDice = RollArray[0];
+  var ModifierArray = RollArray[1].split("+");
+  var NumberOfSides = ModifierArray[0];
+  var Modifier = ModifierArray[1];
+  document.getElementById("rollResult").innerHTML = "Number of Dice: " + NumberOfDice + "<br>" + "Number of Sides: " + NumberOfSides + "<br>" + "Modifier: " + Modifier;
 }
